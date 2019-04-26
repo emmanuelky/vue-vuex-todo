bd2b0eb00d7aa1646a0c75214188f9478d2cd0ba
Emmanuel Akhere Iyere <iyereemmanuelakhere@gmail.com>
2019-04-26 10:37:33 +0200

    created filtertodo component

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3>Add Todo</h3>
-    <form>
+    <form @submit="onSubmit">
       <input type="text" v-model="title" placeholder="Add Todo.....">
       <input type="submit" value="submit">
     </form>
@@ -13,8 +13,17 @@ import { mapActions } from "vuex";
 
 export default {
   name: "AddTodo",
+  data() {
+    return {
+      title: ""
+    };
+  },
   methods: {
-    ...mapActions(["addTodo"])
+    ...mapActions(["addTodo"]),
+    onSubmit(e) {
+      e.preventDefault();
+      this.addTodo(this.title);
+    }
   }
 };
 </script>
